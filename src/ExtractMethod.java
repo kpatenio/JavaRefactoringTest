485f8290e0e93068e4678c18370c25a45daaab1e
kpatenio <patenio@ualberta.ca>
2020-05-20 12:04:13 -0700

    added 17 and rob

--- src/ExtractMethod.java
+++ src/ExtractMethod.java
@@ -7,5 +7,9 @@ public class ExtractMethod {
         System.out.println("Joe");
         System.out.println(1);
         System.out.println(15);
+        System.out.println(17);
+        System.out.println("Rob");
+        // Attempt to see if any refactorings/conflicts detected before extraction and
+        // adding these new lines above (17 and Rob)
     }
 }
