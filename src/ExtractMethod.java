dd41778e19bfc16dc6fbcfd7c9f083c5b8da07e4
kpatenio <patenio@ualberta.ca>
2020-05-20 11:59:37 -0700

    extraction attempt

--- src/ExtractMethod.java
+++ src/ExtractMethod.java
@@ -3,6 +3,10 @@
 public class ExtractMethod {
 
     public static void printThings() {
+        printNumberNames();
+    }
+
+    public static void printNumberNames() {
         System.out.println("Bob");
         System.out.println("Joe");
         System.out.println(1);
