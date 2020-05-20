bc4828eedfa90ce5733fdca3279ad78d835f242a
kpatenio <patenio@ualberta.ca>
2020-05-20 11:17:50 -0700

    first commit - extractmethod

---
+++ src/ExtractMethod.java
@@ -0,0 +1,11 @@
+//
+
+public class ExtractMethod {
+
+    public static void printThings() {
+        System.out.println("Bob");
+        System.out.println("Joe");
+        System.out.println(1);
+        System.out.println(15);
+    }
+}
